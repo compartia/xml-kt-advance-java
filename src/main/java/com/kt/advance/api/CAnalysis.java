f1dca083f3063cefbd16f0658d725a39515cbb77
Artem Zaborskii <compartia@gmail.com>
2018-03-19 19:58:18 +0700

    api update

--- src/main/java/com/kt/advance/api/CAnalysis.java
+++ src/main/java/com/kt/advance/api/CAnalysis.java
@@ -1,16 +1,17 @@
 package com.kt.advance.api;
 
 import java.io.File;
-import java.util.Set;
+import java.util.Collection;
 
 import javax.xml.bind.JAXBException;
 
 public interface CAnalysis {
 
-    Set<CApplication> getApps();
+    Collection<CApplication> getApps();
 
     void read() throws JAXBException;
 
     String relativize(File f);
 
+    CApplication getAppByBaseDir(File baseDir);
 }
