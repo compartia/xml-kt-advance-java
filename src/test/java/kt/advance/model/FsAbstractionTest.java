9159167dd40eff52275483aff70732c09393938e
Artem Zaborskiy <compartia@gmail.com>
2018-08-15 14:41:50 +0200

    Unzipping  📦 semantics.tar.gz

--- src/test/java/kt/advance/model/FsAbstractionTest.java
+++ src/test/java/kt/advance/model/FsAbstractionTest.java
@@ -3,6 +3,7 @@ package kt.advance.model;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 
 import javax.xml.bind.JAXBException;
@@ -11,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.kt.TestMode;
+import com.kt.advance.Util;
 import com.kt.advance.api.FsAbstraction;
 import com.kt.advance.xml.model.FsAbstractionImpl;
 
@@ -31,6 +33,33 @@ public class FsAbstractionTest {
 
         final Collection<File> xmls = fsAbstraction.listXMLs(FsAbstraction.PRD_SUFFIX);
         assertEquals(3, xmls.size());
+    }
+
+    @Test
+    public void testListSemanticsArchives() throws JAXBException {
+
+        final File basedir = new File(getClass().getClassLoader().getResource("xmls/p1").getFile());
+
+        // Create a file system abstraction
+        final FsAbstractionImpl fsAbstraction = new FsAbstractionImpl(basedir);
+
+        final Collection<File> files = fsAbstraction.listSemanticsArchives();
+        assertEquals(1, files.size());
+    }
+
+    @Test
+    public void testExtractSemanticsArchives() throws IOException {
+
+        final File basedir = new File(getClass().getClassLoader().getResource("xmls/p1").getFile());
+
+        // Create a file system abstraction
+        final FsAbstractionImpl fsAbstraction = new FsAbstractionImpl(basedir);
+
+        final Collection<File> files = fsAbstraction.listSemanticsArchives();
+        assertEquals(1, files.size());
+
+        final File inFile = files.iterator().next();
+        Util.unzipSemanticsTarGz(inFile);
     }
 
 }
