da82478744d6639139b1b61e19612c461b67d00e
Artem Zaborskiy <compartia@gmail.com>
2018-08-15 16:33:48 +0200

    Fix Sonar complains (#25)
    * Fix Sonar complains
    * Code clean-up

--- src/main/java/com/kt/advance/xml/XmlNamesUtils.java
+++ src/main/java/com/kt/advance/xml/XmlNamesUtils.java
@@ -23,30 +23,12 @@
  */
 package com.kt.advance.xml;
 
-import java.io.File;
-
+@Deprecated
 public class XmlNamesUtils {
+    private XmlNamesUtils() {
+    }
 
     public static final String XML_EXT = "xml";
-
-    public static File replaceSuffix(File file, String oldSuffix, String newuffix) {
-        final String name = file.getName();
-        final String newName = name.replace(oldSuffix, newuffix);
-        return new File(
-            file.getParentFile(),
-            newName);
-    }
-
-    public static File xmlFilename(final File file, final String filePattern, String suff) {
-        final StringBuilder sb = new StringBuilder()
-                .append(filePattern)
-                .append(suff)
-                .append('.')
-                .append(XmlNamesUtils.XML_EXT);
-        return new File(
-            file.getParentFile(),
-            sb.toString());
-    }
 
     public static String xmlSuffix(String postfix) {
         return postfix + "." + XmlNamesUtils.XML_EXT;
